373b1d86e54ad3b6a1775599e1a904e8130f13f7
aFarkas <info@corrupt-system.de>
2015-07-29 11:45:35 +0200

    add utils

--- tests/unit/boilerplate.js
+++ tests/unit/boilerplate.js
@@ -2,8 +2,9 @@ define([
 	'intern!object',
 	'intern/chai!expect',
 	'intern/order!component-helpers/js/object-assign',
-	'intern/order!https://boffinhouse.github.io/rawblock/sources/js/libs/rb-life.js',
 	'intern/order!https://boffinhouse.github.io/rawblock/sources/js/libs/dom.js',
+	'intern/order!https://boffinhouse.github.io/rawblock/sources/js/libs/rb-utils.js',
+	'intern/order!https://boffinhouse.github.io/rawblock/sources/js/libs/rb-life.js',
 	'intern/order!sources/js/modules/rb_boilerplate',
 ], function(registerSuite, expect) {
 	var widgetElement, widget;
