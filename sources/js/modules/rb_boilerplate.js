116319c71790a23ec70f8b39ffac7043faba951c
aFarkas <info@corrupt-system.de>
2015-08-28 08:20:13 +0200

    add initWrite hook

--- sources/js/modules/rb_boilerplate.js
+++ sources/js/modules/rb_boilerplate.js
@@ -7,23 +7,32 @@
 	return rb.life.Widget.extend('boilerplate', {
 		defaults: {},
 
+		/* init use init to construct/prepare your widget, but organize your read/write cycles (start layout reads in init and write to DOM in initWrite)*/
 		init: function(element){
 			this._super(element);
 
-			console.log(this.element, this.options);
+			console.log(this.element, this.$element, this.options);
 		},
+
+		/* initWrite is invoked after init and is optimized for DOM writes */
+		initWrite: function(){
+
+		},
+
 		/*
 		onceAttached is invoked in the same cycle as init, but after all other current widgets are also initialized
-		use for tight dependencies between different widgets or to organize read/write cycles (start reads in init and write in onceAttached)
+		use for tight dependencies between different widgets
 		*/
 		onceAttached: function(){
 
 		},
+
 		setOption: function(name, value){
 			this._super(name, value);
 		},
+
 		/*
-		 attached/detached are invoked everytime an element is inserted or removed from the document.
+		 attached/detached are invoked every time the widget element is inserted or removed from the document.
 		 example use to bind/unbind global events (resize/scroll) or timers.
 		 but only use if really needed (+ not suitable for widgets, that can a lot of instances (30+) on one page at the same time):
 		 the pure existence of one of this callback methods can slow down 'remove' performance
@@ -33,6 +42,7 @@
 
 		},
 		*/
+
 		/*
 		detached: function(){
 
