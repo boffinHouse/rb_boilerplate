4072a6bbe4353f651d0afed3b8dfbbed886ee72f
aFarkas <info@corrupt-system.de>
2015-10-10 00:11:42 +0200

    ...

--- sources/js/modules/rb_boilerplate.js
+++ sources/js/modules/rb_boilerplate.js
@@ -4,56 +4,75 @@
 	var rb = window.rb;
 	var $ = rb.$;
 
-	var Boilerplate = rb.Widget.extend('boilerplate', {
-		defaults: {
-			debug: true,
-		},
+	var Boilerplate = rb.Widget.extend('boilerplate',
+		/** @lends rb.widgets.boilerplate.prototype */
+		{
 
-		/* use init to construct/prepare/create your widget, but organize your read/write cycles (start layout reads in init and write to DOM using rb.rAF (see _writeLayout))*/
-		init: function(element){
-			this._super(element);
+			/**
+			 * @static
+			 * @property {Object} defaults
+			 * //@property {Boolean} defaults.debug=true Log debug messages
+			 */
+			defaults: {
+				debug: true,
+			},
+			/* use init to construct/prepare/create your widget, but organize your read/write cycles (start layout reads in init and write to DOM using rb.rAF (see _writeLayout))*/
+			/**
+			 * @classdesc Class component to create a boilerplate.
+			 * @constructs
+			 * @param element
+			 * @example
+			 * <div class="rb-boilerplate js-rb-life" data-module="boilerplate">
+			 *     <div></div>
+			 * </div>
+			 * @example
+			 * rb.$('.rb-boilerplate').rbWidget();
+			 */
+			init: function(element){
+				this._super(element);
 
-			this.log(this.element, this.$element, this.options, this);
+				this.log(this.element, this.$element, this.options, this);
 
-			//this._writeLayout = rb.rAF(this._writeLayout, {that: this});
+				//this._writeLayout = rb.rAF(this._writeLayout, {that: this});
 
-			//trigger events
-			//this._trigger();
-		},
-		/* events simple event bindings with event delegation */
-		events: {
-			//'click .{name}-btn': 'methodName',
-			//'click .{name}-btn': function(e){
-			//	this.log(this);
+				//trigger events
+				//this._trigger();
+			},
+			/* events simple event bindings with event delegation */
+			events: {
+				//'click .{name}-btn': 'methodName',
+				//'click .{name}-btn': function(e){
+				//	this.log(this);
+				//},
+			},
+			/* statics can reached over Boilerplate.regFoo */
+			statics: {
+				//regFoo: /foo/g
+			},
+			setOption: function(name, value){
+				this._super(name, value);
+			},
+
+			/*
+			 attached/detached are invoked every time the widget element is inserted or removed from the document.
+			 use to bind/unbind global events (resize/scroll) or timers.
+			 but only use if really needed (+ not suitable for widgets, that have a lot of instances (100+) on one page at the same time):
+			 the pure existence of one of this callback methods can slow down 'remove' performance
+			 */
+			//attached: function(){
+			//
 			//},
-		},
-		/* statics can reached over Boilerplate.regFoo */
-		statics: {
-			//regFoo: /foo/g
-		},
-		setOption: function(name, value){
-			this._super(name, value);
-		},
 
-		/*
-		 attached/detached are invoked every time the widget element is inserted or removed from the document.
-		 use to bind/unbind global events (resize/scroll) or timers.
-		 but only use if really needed (+ not suitable for widgets, that have a lot of instances (100+) on one page at the same time):
-		 the pure existence of one of this callback methods can slow down 'remove' performance
-		 */
-		//attached: function(){
-		//
-		//},
-
-		//detached: function(){
-		//
-		//},
-		/* you can use getters/setters */
-		//get yourGetter(){
-		//
-		//},
-		//set yourSetter(value){
-		//
-		//}
-	});
+			//detached: function(){
+			//
+			//},
+			/* you can use getters/setters */
+			//get yourGetter(){
+			//
+			//},
+			//set yourSetter(value){
+			//
+			//}
+		}
+	);
 })();
